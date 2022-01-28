46a05494331193e4d3f6a6744b14cd5adab53e2e
kudzai-jalos <kudzai.jalos@younglings.africa>
2022-01-28 13:15:21 +0200

    Complete js refresher

---
+++ NOTES.md
@@ -0,0 +1,9 @@
+# REPL
+R - Read
+E - Eval
+P - Print
+L - Loop
+
+# Execute Files
+Used for apps
+Predictable sequence of steps
--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-const fs = require("fs");
+const fetchData = () => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const data = {
+        status: "Done",
+      };
 
-fs.writeFileSync("hello.txt","Hello NodeJs");
+      resolve(data);
+    }, 1500);
+  });
+
+  return promise;
+};
+
+setTimeout(() => {
+  console.log("Hello");
+  fetchData().then(data=>{
+    console.log(data)
+    return fetchData();
+  }).then(data=>{
+    console.log(data.status)
+  })
+}, 1000);
+
+console.log("Hi");
