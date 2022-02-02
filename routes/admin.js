0b929a4a4dd73461953265d7aeb430473e6bb1b0
unknown <kudzaijalos@gmail.com>
2022-02-02 14:09:50 +0200

    Normalize root dir

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
+
 const router = express.Router();
 
+const rootDir = require('../util/path');
+
 router.route("/add-product").get((req,res,next)=>{
-  res.sendFile(path.join(__dirname,"..","views","add-product.html"));
+  res.sendFile(path.join(rootDir,"views","add-product.html"));
 }).post((req,res,next)=>{
   console.log(req.body);
   res.redirect('/');
