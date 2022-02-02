3e6283550dfd8523194adc496d9c40dafd76f872
unknown <kudzaijalos@gmail.com>
2022-02-02 13:37:57 +0200

    Filter routes and seprate them into different files

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
 // import express module
 const express = require('express');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 // Create an express application
 const app = express();
 
 // Add middleware
-app.use((req,res,next)=>{
-  console.log("In the middleware!");
-  next(); // Allow request to continue to the next middleware function
-});
 
-app.use((req,res,next)=>{
-  console.log("In the 2nd middleware!!");
-  // ....
-  res.send("<h1>Request received.</h1>");
-});
+app.use(require('body-parser').urlencoded({extended:false}));
 
+app.use(adminRoutes);
+app.use(shopRoutes);
 // Listen for incoming requests
 app.listen(3000,()=>{
   console.log("Server running on port 3000");
